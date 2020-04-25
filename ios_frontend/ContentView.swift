a57e5e51954ea7ad89b27480e0b829b5d19b8161
andrey <andrey@kabylin.ru>
2020-04-25 21:27:42 +0700

    Initial Commit

---
+++ ios_frontend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_frontend
+//
+//  Created by Андрей Кабылин on 25.04.2020.
+//  Copyright © 2020 RedGoosePaws. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_frontendTests/ios_frontendTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_frontendTests.swift
+//  ios_frontendTests
+//
+//  Created by Андрей Кабылин on 25.04.2020.
+//  Copyright © 2020 RedGoosePaws. All rights reserved.
+//
+
+import XCTest
+@testable import ios_frontend
+
+class ios_frontendTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
